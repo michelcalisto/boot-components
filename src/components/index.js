97af800b51c98f60abec5458d44a1cf29eee7404
Michel Calisto <calisto.michel@gmail.com>
2023-03-28 09:58:18 -0300

    feat: added breadcrumbs component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,17 @@
-import FlButton from './Button/FlButton.vue'
-import FlBut from './Button/FlBut.vue'
-import FlBreadcrumbs from './FlBreadcrumbs'
+// import FlButton from './Button/FlButton.vue'
+// import FlBut from './Button/FlBut.vue'
+// import FlBreadcrumbs from './FlBreadcrumbs'
+import BootBreadcrumbs from './Breadcrumbs'
 
 // export default { FlBut }
-export default { FlButton, FlBut, FlBreadcrumbs }
+// export default 
+// { 
+//     FlButton, 
+//     FlBut, 
+//     FlBreadcrumbs 
+// }
+
+export default 
+{ 
+    BootBreadcrumbs 
+}
