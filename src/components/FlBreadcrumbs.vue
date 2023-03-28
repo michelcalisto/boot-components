a7324ab7719dfe621f94c5fcfa4996bdad8457d1
Michel Calisto <calisto.michel@gmail.com>
2023-03-28 09:33:03 -0300

    feat: example of components

---
+++ src/components/FlBreadcrumbs.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    items: Array,
+});
+</script>
+
+<template>
+    <nav class="flex font-bold text-gray-700" aria-label="Breadcrumb">
+        <ol v-for="(item, index) in items" :key="index" class="list-reset flex">
+            <li v-if="item.url"><a :href="item.url" class="text-gray-400 hover:text-gray-500">{{ __(item.label) }}</a></li>
+            <li v-else class="text-gray-600">{{ __(item.label) }}</li>
+            <li v-if="index < (items.length - 1)"><span class="text-gray-300 mx-2">/</span></li>
+        </ol>
+    </nav>
+</template>
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import FlButton from './Button/FlButton.vue'
+import FlBut from './Button/FlBut.vue'
+import FlBreadcrumbs from './FlBreadcrumbs'
+
+// export default { FlBut }
+export default { FlButton, FlBut, FlBreadcrumbs }
