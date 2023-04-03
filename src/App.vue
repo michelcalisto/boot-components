985ca78fb79a2d2ad993e14c926fc47f84f81d71
Michel Calisto <calisto.michel@gmail.com>
2023-04-03 15:29:59 -0400

    refactor app.vue

--- src/App.vue
+++ src/App.vue
@@ -15,28 +15,5 @@ const colors = computed(() => { return {
 </script>
 
 <template>
-  {{ colors.slash }}
   <Breadcrumb :items="crumbs" :colors="colors" />
 </template>
-
-<!-- <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script> -->
-
-<!-- <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> -->
